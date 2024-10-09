e257793f43292a97a65252619744a6710240c1dc
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-10-09 09:48:04 -0500

    added files

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import './App.css'
+
+import Header from '/pages/Header';
+
+function App() {
+
+  return (
+    <Header />
+    
+  );
+}
+
+export default App
